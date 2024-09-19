3c4a304ef3b0f559409022e0cd402f5d5646967d
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-09-19 20:15:55 +0200

    start_server_MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import connectDB from './config/dbConfig.js'
 import 'dotenv/config'
-import contactRoutes from './routes/contactRoutes.js'
+import contactRoutes from './routers/contactRoutes.js'
 
 const app = express()
 
@@ -14,7 +14,7 @@ connectDB()
 // Routes
 app.use('/api', contactRoutes)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
