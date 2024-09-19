ab149adac5794b09389017ca072f00e3cc6abc27
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-09-19 20:15:55 +0200

    start_server_MongoDB

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,15 +1,11 @@
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import 'dotenv/config'
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
     })
     console.log('MongoDB connected')
   } catch (error) {
