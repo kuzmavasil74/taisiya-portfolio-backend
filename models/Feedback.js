9f856942bea0765e1be442e2e76873ffaa138608
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2025-04-12 11:09:37 +0200

    some updates 12.04.25

--- models/Feedback.js
+++ models/Feedback.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const feedbackSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  rating: { type: Number, required: true, min: 1, max: 5 },
+  rating: { type: Number, required: true, min: 1, max: 10 },
   comment: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 })
