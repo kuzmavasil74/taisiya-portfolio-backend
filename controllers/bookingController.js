771247661fdcb752d6c3aef818d6515b06cba421
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2026-02-20 14:58:57 +0100

    change_bookingController_20.02.26(1)

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -254,6 +254,9 @@ export const getAvailableSlots = async (req, res) => {
             Interval.fromDateTimes(bStart, bEnd)
           )
         })
+        console.log('Now:', now.toISO())
+        console.log('Slot:', slotStart.toISO())
+        console.log('Conflict:', conflict)
 
         // Пропускаємо минулі слоти
         if (slotStart > now) {
@@ -261,9 +264,6 @@ export const getAvailableSlots = async (req, res) => {
             time: slotStart.toFormat('HH:mm'),
             available: !conflict,
           })
-          console.log('Now:', now.toISO())
-          console.log('Slot:', slotStart.toISO())
-          console.log('Conflict:', conflict)
         }
       }
     }
